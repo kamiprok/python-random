76688d70f9ce9ae5c2a6fb32fb9057157bcb5089
kamiprok <kamiprok@gmail.com>
2020-07-21 23:24:13 +0200

    display data from db on website

--- myflask.py
+++ myflask.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from datetime import datetime
 from random import randrange
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
@@ -19,6 +20,17 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/data')
+def data():
+    client = MongoClient('MongoDBConnectionString')
+    db = client.MongoDB
+    collection = db.collection
+    list = []
+    for i in collection.find({}):
+        list.append(i)
+    return render_template('data.html', len=len(list), list=list)
+
+
 app.run(host='0.0.0.0', port=8080)
 
 # C:\Users\KAMSON-PC\PycharmProjects\python-random\set FLASK_APP=myflask.py
@@ -27,3 +39,4 @@ app.run(host='0.0.0.0', port=8080)
 # py -m flask run
 # flask run --host=0.0.0.0
 # with app.run() you can run flask app my py myflask.py instead of setting variable and running flask run
+# data needs connection string to db and pulls data from db
--- mymongodb.py
+++ mymongodb.py
@@ -1,8 +1,4 @@
 from pymongo import MongoClient
-from pprint import pprint
-from getpass import getpass
-
-# dbpass = getpass('dbpass: ')
 
 client = MongoClient('MongoDBConnectionString')
 
@@ -11,26 +7,28 @@ client = MongoClient('MongoDBConnectionString')
 # pprint(serverStatusResult)
 
 db = client.MongoDB
-pprint(f'db = {db}')
+print(f'db = {db.name}')
 collection = db.collection
-pprint(f'collection = {collection}')
-document = {
-    'key': 'value'
-}
-pprint(f'document = {document}')
+print(f'collection = {collection.name}')
+
+document = {'x': 1}
+
+print(f'document = {document}')
 # collection.insert_one(document)
 
-pprint(f'find_one: {db.collection.find_one({"key": "value"})}')
-pprint(f'collection_names: {db.collection_names}')
-pprint(f'count: {db.collection.find({"key": "variable"}).count()}')
-pprint(f'kroos: {db.kroos.find({})}')  # sets cursor to mongodb object
+print(f'find_one: {collection.find_one({"key": "value"})}')
+
+print(f'count: {collection.find({"key": "variable"}).count()}')
+print(f'count: {collection.count_documents({})}')
+
+for i in collection.find({}):
+    print(i)
+
+print(f'kroos: {db.kroos.find({})}')  # sets cursor to mongodb object
 for i in db.kroos.find():
     print(i)
-blushed = db.kroos.find_one({'_id': '1'})
+
+blushed = db.kroos.find_one({'_id': 1})
 blushed_val = blushed['blushed']  # value of blushed
+print(blushed)
 print(blushed_val)
-blushed_val += 1
-print(blushed_val)
-db.kroos.find_one_and_update({'_id': '1'}, {'$inc': {'blushed': 1}})
-blushed = db.kroos.find_one({'_id': '1'})
-pprint(blushed)
