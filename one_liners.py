bb9e2b4a1f4225ebf68182a26a232d3e7a99cd7b
kamiprok <kamiprok@gmail.com>
2020-08-06 01:22:10 +0200

    formated for better readability, default values and typing error catching

--- one_liners.py
+++ one_liners.py
@@ -1,4 +1,5 @@
 import os
+import ast
 
 
 def press_any_key():
@@ -10,11 +11,13 @@ def clear():
 
 
 def menu():
+    clear()
+    print('10 Awesome Pythonic One Lienrs\n')
     print('Menu:\n')
     print('1. Swap two variables')
     print('2. Multiple variable assignment')
     print('3. Sum over every second element of a list')
-    print('0. Exit\n')
+    print('\n0. Exit\n')
     choice = int(input('Choice: '))
     if choice == 0:
         press_any_key()
@@ -38,34 +41,80 @@ def menu():
 
 def swap():
     clear()
-    print('Swapping the values of variables by simply swapping positions on assignment:\n')
-    a = int(input('a = '))
-    b = int(input('b = '))
-    print('\n# Swapping:\n>> a, b = b, a')
+    print("""Let's start with a classic: swapping the values of variables
+by simply swapping positions on assignment - the most
+intuitive way in my opinion. No need to use a temporary
+variable. It even works with more than two variables.\n""")
+    try:
+        a = int(input('Input a: '))
+        b = int(input('Input b: '))
+    except:
+        print('Incorrect input, using default values:')
+        a = 1
+        b = 2
+    print(f'\n# a = {a}; b = {b}')
+    print('a, b = b, a')
     a, b = b, a
-    print(f'\nResult:\na = {a}\nb = {b}')
+    print(f'# print(a,b) >> {a} {b}')
 
 
 def multi():
     clear()
-    print('Swapping variables is actually a special case of Pythons ability to assign multiple variables at once:\n')
-    print('a, b, *c = [1,2,3,4,5]')
-    print('# print(a,b,c) >> 1 2 [3, 4, 5]')
+    print('''Swapping variables is actually a special case of Pythons
+ability to assign multiple variables at once. Here you can
+use it to assign list elements to the given variables,
+which is also called unpacking. The * will do packing
+the remaining values again, which results in a sublist for
+c. It even works for every other position of * (e.g. the
+beginning or middle part of the list).\n''')
+    try:
+        var_list = input('Input list [1,2,3,4,5]: ')
+        var_list = ast.literal_eval(var_list)
+        if not isinstance(var_list, list):
+            print('Using default values [1,2,3,4,5]')
+            var_list = []
+            var_list = [1, 2, 3, 4, 5]
+    except:
+        if isinstance(var_list, str):
+            print('Using default values [1,2,3,4,5]')
+        else:
+            print('Incorrect input, using default values [1,2,3,4,5]')
+        var_list = []
+        var_list = [1,2,3,4,5]
+    a, b, *c = var_list
+    print(f'\na, b, *c = {var_list}')
+    print(f'# print(a,b,c) >> {a, b, c}')
 
 
 def sum_second():
     clear()
-    print("Sum just adds the items of every given iterable."
-          "\nThe extended slicing syntax [::] is used here to return every second element."
-          "\nYou can read it as [start : stop : step], so [1::2] translates to start with the element of index 1 (second element),"
-          "\ndon't stop until the list ends (no argument given for second parameter) and always take 2 steps:\n")
-    print('# a = [1,2,3,4,5,6]')
+    print('''No need for a special reduce function here, sum just 
+adds the items of every given iterable. The extended 
+slicing syntax [::] is used here to return every second 
+element. You can read it as [start : stop : step], so [1::2] 
+translates to start with the element of index 1 (second 
+element), don't stop until the list ends (no argument given 
+for second parameter) and always take 2 steps.''')
+    try:
+        var_list = input('\nInput list [1,2,3,4,5,6]: ')
+        var_list = ast.literal_eval(var_list)
+        if not isinstance(var_list, list):
+            print('Using default values [1,2,3,4,5,6]')
+            var_list = []
+            var_list = [1, 2, 3, 4, 5, 6]
+    except:
+        if isinstance(var_list, str):
+            print('Using default values [1,2,3,4,5,6]')
+        else:
+            print('Incorrect input, using default values [1,2,3,4,5,6]')
+        var_list = []
+        var_list = [1, 2, 3, 4, 5, 6]
+    print(f'\n# a = {var_list}')
+    s = sum(var_list[1::2])
     print('s = sum(a[1::2])')
-    print('# print(s) >> 12')
+    print(f'# print(s) >> {s}')
 
 
-clear()
-print('Welcome to 10 Awesome Pythonic One Lienrs\n')
 menu()
 # static to active, let user input all variables, ex. sum_second: enter list of vars (default[1,2,3,4,5,6] >
 # if no option or bad input use default, else use user inputs
